e88c692f2a4c21c6c39863160eb56c9e90973fcf
Wei Chang Lin <jackie@Weis-MacBook-Pro.local>
2023-01-01 18:30:40 -0500

    first commit.

---
+++ WaifuOrNot/WaifuOrNotApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaifuOrNotApp.swift
+//  WaifuOrNot
+//
+//  Created by Wei Chang Lin on 2023-01-01.
+//
+
+import SwiftUI
+
+@main
+struct WaifuOrNotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
